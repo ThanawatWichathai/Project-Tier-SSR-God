7cd1e35507d8ab8be5d7e1b351f722194f95b04f
Topthai <top994.top@gmail.com>
2026-02-03 22:57:02 +0700

    add project files

---
+++ S.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    printf( "Hello World" ) ;
+    printf( "\n...My name is Thanawat" ) ;
+    
+
+    return 0 ;
+}//end function
---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    printf( "Hello World" ) ;
+    printf( "\nMy name is Thanawat" ) ;
+    
+
+    return 0 ;
+}//end function
